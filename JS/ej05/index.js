104ba99289bef6d438724ceda924091fa6e27b5a
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2023-07-14 14:32:38 +0200

    Ej 05 (JS)

--- JS/ej05/index.js
+++ JS/ej05/index.js
@@ -1,31 +1,79 @@
-console.log("Javier");
-console.log("Santos");
-console.log("Pudgemon");
-console.log("Andorra");
-console.log("Gratis");
-console.log("1-562-867-5309");
-console.log("javierinventado@yandex.ru");
-console.log("------");
-console.log("Pedro");
-console.log("Torreiro");
-console.log("Calvo");
-console.log("Francia");
-console.log("Premium");
-console.log("1-342-862-5309");
-console.log("otropedroinventado@yandex.ru");
-console.log("------");
-console.log("Manolo");
-console.log("Costas");
-console.log("Ferradás");
-console.log("España");
-console.log("Básico");
-console.log("1-532-367-7609");
-console.log("manoloinventado@yandex.ru");
-console.log("------");
-console.log("Pedro");
-console.log("Costas");
-console.log("Ferradás");
-console.log("España");
-console.log("Premium");
-console.log("981-123-456");
-console.log("pedroinventado@yandex.ru");
+const form = document.getElementById("form");
+const alerta = document.getElementById("alerta");
+const btnCerrarAlerta = document.getElementById("cerrar");
+const btnDetener = document.getElementById("detener");
+const btnPostponer = document.getElementById("postponer");
+
+const audio = new Audio("alarma.mp3");
+
+btnCerrarAlerta.addEventListener("click", () => {
+    alerta.close();
+    audio.pause();
+    audio.currentTime = 0;
+    btnDetener.style.display = "none";
+});
+
+btnPostponer.addEventListener("click", () => {
+    alerta.close();
+    audio.pause();
+    audio.currentTime = 0;
+    manejarAlarma(10);
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    let time = formData.get("time");
+
+    manejarAlarma(time);
+});
+
+function manejarAlarma(time) {
+    if (time === null) {
+        alert("Debes introducir una hora");
+        return;
+    }
+
+    if (time <= 0) {
+        alert("El tiempo no puede ser negativo o cero");
+        return;
+    }
+
+    const alarma = document.getElementById("alarma");
+
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor((time % 3600) / 60);
+    let seconds = time % 60;
+
+    alarma.innerText = `${hours}h ${minutes}m ${seconds}s`;
+
+    form.reset();
+
+    btnDetener.style.display = "block";
+
+    const interval = setInterval(() => {
+        time--;
+
+        hours = Math.floor(time / 3600);
+        minutes = Math.floor((time % 3600) / 60);
+        seconds = time % 60;
+
+        alarma.innerText = `${hours}h ${minutes}m ${seconds}s`;
+    }, 1000);
+
+    const timeout = setTimeout(() => {
+        clearInterval(interval);
+        alerta.showModal();
+        audio.play();
+    }, time * 1000);
+
+    btnDetener.addEventListener("click", () => {
+        clearInterval(interval);
+        clearTimeout(timeout);
+        alarma.innerText = "0h 0m 0s";
+        btnDetener.style.display = "none";
+        btnDetener.removeEventListener("click", () => {});
+    });
+}
--- JS/ej07/index.js
+++ JS/ej07/index.js
@@ -1,51 +1,78 @@
-const form = document.getElementById("form");
-const alerta = document.getElementById("alerta");
-const btnCerrarAlerta = document.querySelector("#alerta button");
+console.log({
+    "nombre": "Javier",
+    "primer_apellido": "Santos",
+    "segundo_apellido": "Pudgemon",
+    "pais": "Andorra",
+    "tipo_cuenta": "Gratis",
+    "telefono": "1-562-867-5309",
+    "email": "javierinventado@yandex.ru"
+})
+console.log("------");
 
-btnCerrarAlerta.addEventListener("click", () => {
-    alerta.close();
-});
+console.log({
+    "nombre": "Pedro",
+    "primer_apellido": "Torreiro",
+    "segundo_apellido": "Calvo",
+    "pais": "Francia",
+    "tipo_cuenta": "Premium",
+    "telefono": "1-342-862-5309",
+    "email": "otropedroinventado@yandex.ru"
+})
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+console.log("------");
 
-    const formData = new FormData(form);
+console.log({
+    "nombre": "Manolo",
+    "primer_apellido": "Costas",
+    "segundo_apellido": "Ferradás",
+    "pais": "España",
+    "tipo_cuenta": "Básico",
+    "telefono": "1-532-367-7609",
+    "email": "manoloinventado@yandex.ru",
+})
 
-    let time = formData.get("time");
+console.log("------");
 
-    if (time === null) {
-        alert("Debes introducir una hora");
-        return;
-    }
+console.log({
+    "nombre": "Pedro",
+    "primer_apellido": "Costas",
+    "segundo_apellido": "Ferradás",
+    "pais": "España",
+    "tipo_cuenta": "Premium",
+    "telefono": "981-123-456",
+    "email": "pedroinventado@yandex.ru"
+})
 
-    if (time <= 0) {
-        alert("El tiempo no puede ser negativo o cero");
-        return;
-    }
+console.log("------");
 
-    const alarma = document.getElementById("alarma");
-
-    let hours = Math.floor(time / 3600);
-    let minutes = Math.floor((time % 3600) / 60);
-    let seconds = time % 60;
-
-    alarma.innerText = `${hours}h ${minutes}m ${seconds}s`;
-
-    form.reset();
-
-    const interval = setInterval(() => {
-        time--;
-
-        hours = Math.floor(time / 3600);
-        minutes = Math.floor((time % 3600) / 60);
-        seconds = time % 60;
-
-        alarma.innerText = `${hours}h ${minutes}m ${seconds}s`;
-    }, 1000);
-
-    setTimeout(() => {
-        clearInterval(interval);
-
-        alerta.showModal();
-    }, time * 1000);
-});
+console.log("Javier");
+console.log("Santos");
+console.log("Pudgemon");
+console.log("Andorra");
+console.log("Gratis");
+console.log("1-562-867-5309");
+console.log("javierinventado@yandex.ru");
+console.log("------");
+console.log("Pedro");
+console.log("Torreiro");
+console.log("Calvo");
+console.log("Francia");
+console.log("Premium");
+console.log("1-342-862-5309");
+console.log("otropedroinventado@yandex.ru");
+console.log("------");
+console.log("Manolo");
+console.log("Costas");
+console.log("Ferradás");
+console.log("España");
+console.log("Básico");
+console.log("1-532-367-7609");
+console.log("manoloinventado@yandex.ru");
+console.log("------");
+console.log("Pedro");
+console.log("Costas");
+console.log("Ferradás");
+console.log("España");
+console.log("Premium");
+console.log("981-123-456");
+console.log("pedroinventado@yandex.ru");
